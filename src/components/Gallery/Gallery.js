080d0655b36ed8b03f0ac34694d7aadf0e7f4329
Amir Zalka <amirzalka@gmail.com>
2018-03-05 18:11:11 +0200

    first version

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -3,6 +3,8 @@ import PropTypes from 'prop-types';
 import axios from 'axios';
 import Image from '../Image';
 import './Gallery.scss';
+import Slider from 'react-image-slider';
+//import './Slider';
 
 class Gallery extends React.Component {
   static propTypes = {
@@ -11,10 +13,19 @@ class Gallery extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      images: [],
-      galleryWidth: this.getGalleryWidth()
-    };
+        this.removeItem = this.removeItem.bind(this);
+        this.startSlider = this.startSlider.bind(this);
+        this.stopSlider = this.stopSlider.bind(this);
+        this.nextImg = this.nextImg.bind(this);
+        this.handleScroll = this.handleScroll.bind(this);
+        this.state = {
+           images: [],
+           galleryWidth: this.getGalleryWidth(),
+           showSlider: false,
+           currentIndex: '',
+           imageToStartUrl: '',
+           page: 1
+        };
   }
 
   getGalleryWidth(){
@@ -25,7 +36,7 @@ class Gallery extends React.Component {
     }
   }
   getImages(tag) {
-    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=100&format=json&nojsoncallback=1`;
+    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&page=${this.state.page}&per_page=100&format=json&nojsoncallback=1`;
     const baseUrl = 'https://api.flickr.com/';
     axios({
       url: getImagesUrl,
@@ -40,7 +51,9 @@ class Gallery extends React.Component {
           res.photos.photo &&
           res.photos.photo.length > 0
         ) {
-          this.setState({images: res.photos.photo});
+
+          this.setState({images: this.state.images.concat(res.photos.photo)});
+            console.log(this.state.images);
         }
       });
   }
@@ -50,18 +63,65 @@ class Gallery extends React.Component {
     this.setState({
       galleryWidth: document.body.clientWidth
     });
+    window.addEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll () {
+    var d = document.documentElement;
+    var offset = d.scrollTop + window.innerHeight;
+    var height = d.offsetHeight;
+
+    if (offset === height) {
+        this.setState({page: ++this.state.page});
+        console.log('At the bottom');
+        console.log(this.state.page);
+        this.getImages(this.props.tag);
+    }
   }
 
   componentWillReceiveProps(props) {
     this.getImages(props.tag);
   }
 
+  removeItem (i) {
+    var UpdatedImgArr = this.state.images.slice(); //making shallow copy of the array
+    UpdatedImgArr.splice(i, 1); //removing the item
+    this.setState({images: UpdatedImgArr}); //update state
+  }
+
+  startSlider(index,url) {
+    this.setState({showSlider: true});
+    this.setState({currentIndex: index});
+    this.setState({imageToStartUrl: url});
+  }
+
+  stopSlider() {
+    this.setState({showSlider: false});
+  }
+
+  nextImg =() => {
+    this.setState({currentIndex: ++this.state.currentIndex});
+    this.setState({imageToStartUrl: this.refs.child.urlFromDto(this.state.images[this.state.currentIndex])});
+    console.log(this.state.currentIndex);
+    console.log(this.state.imageToStartUrl);
+  }
+
+
   render() {
     return (
       <div className="gallery-root">
-        {this.state.images.map(dto => {
-          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth}/>;
+        {this.state.images.map((dto, index) => {
+          return <Image ref="child" key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth} index={index} removeItem={this.removeItem} startSlider={this.startSlider}/>;
         })}
+        <div className={this.state.showSlider ? '' : 'hidden'}>
+            <div className="slider">
+                <img className="slider-img" src={this.state.imageToStartUrl}/>
+              <div className="actions">
+                <button className="close" onClick={this.stopSlider}>Close</button>
+                <button className="next" onClick={this.nextImg}>Next</button>
+              </div>
+            </div>
+        </div>
       </div>
     );
   }
--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -11,10 +11,16 @@ class Image extends React.Component {
 
   constructor(props) {
     super(props);
-    this.calcImageSize = this.calcImageSize.bind(this);
-    this.state = {
-      size: 200
-    };
+        this.calcImageSize = this.calcImageSize.bind(this);
+        this.deleteItem = this.deleteItem.bind(this);
+        this.rotateImg = this.rotateImg.bind(this);
+        this.openSlider = this.openSlider.bind(this);
+        this.state = {
+            size: 200,
+            sizePercent: 0,
+            rotationImg: 0,
+            rotationBtn: 0,
+        };
   }
 
   calcImageSize() {
@@ -22,8 +28,12 @@ class Image extends React.Component {
     const targetSize = 200;
     const imagesPerRow = Math.round(galleryWidth / targetSize);
     const size = (galleryWidth / imagesPerRow);
+    const sizePercent = (size*100 / galleryWidth);
     this.setState({
-      size
+        size:size
+    });
+    this.setState({
+        sizePercent:sizePercent
     });
   }
 
@@ -35,20 +45,43 @@ class Image extends React.Component {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
 
+  deleteItem () {
+    var index = parseInt(this.props.index);
+    this.props.removeItem(index);
+  }
+
+  rotateImg () {
+    let Rotate = this.state.rotationImg + 90;
+    this.setState({
+        rotationImg: Rotate
+    });
+    this.setState({
+        rotationBtn: Rotate*(-1)
+    })
+  }
+
+  openSlider () {
+     var index = parseInt(this.props.index);
+     var url = this.urlFromDto(this.props.dto);
+     this.props.startSlider(index,url);
+  }
+
+
   render() {
     return (
       <div
         className="image-root"
         style={{
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
-          width: this.state.size + 'px',
-          height: this.state.size + 'px'
-        }}
+          width: this.state.sizePercent + '%',
+          height: this.state.size + 'px',
+          transform: `rotate(${this.state.rotationImg}deg)`,
+          }}
         >
-        <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
-          <FontAwesome className="image-icon" name="expand" title="expand"/>
+        <div style={{transform: `rotate(${this.state.rotationBtn}deg)`}}>
+          <FontAwesome className="image-icon" name="sync-alt" title="rotate" onClick={this.rotateImg}/>
+          <FontAwesome className="image-icon" name="trash-alt" title="delete" onClick={this.deleteItem}/>
+          <FontAwesome className="image-icon" name="expand" title="expand" onClick={this.openSlider}/>
         </div>
       </div>
     );
