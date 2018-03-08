15d06796a075a96da37d6452c6132b7ba8e5da6a
galezra <galezra@gmail.com>
2018-03-08 20:25:30 +0200

    Add rotate image & delete & expand buttons

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
+import LightBox from 'react-image-lightbox';
 import './Image.scss';
+
 
 class Image extends React.Component {
   static propTypes = {
@@ -12,8 +14,13 @@ class Image extends React.Component {
   constructor(props) {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
+    this.rotateImage = this.rotateImage.bind(this);
     this.state = {
-      size: 200
+      size: 200,
+      rotation: 0,
+      photoIndex :0,
+      isGalleryMode: false
+
     };
   }
 
@@ -35,24 +42,68 @@ class Image extends React.Component {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
 
+  rotateImage(){
+    let newRotation = this.state.rotation + 90;
+    if(newRotation >= 360){
+      newRotation =- 360;
+    }
+    this.setState({
+      rotation: newRotation
+    })
+  }
+
+  deleteImage(){
+    const id = this.props.dto.id;
+    const newImagesArr = this.props.images.filter(image => image.id !== id);
+    this.props.updateNewGallery(newImagesArr);
+  }
+
+  findIndexById(){
+    const id = this.props.dto.id;
+    return this.props.images.findIndex(image => image.id === id);
+  }
+
   render() {
+    const {isGalleryMode , photoIndex} = this.state;
     return (
       <div
-        className="image-root"
-        style={{
+        className="image-root"  style={{
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
           width: this.state.size + 'px',
-          height: this.state.size + 'px'
+          height: this.state.size + 'px',
+          transform: `rotate(${this.state.rotation}deg)`
         }}
-        >
-        <div>
-          <FontAwesome className="image-icon" name="sync-alt" title="rotate"/>
-          <FontAwesome className="image-icon" name="trash-alt" title="delete"/>
-          <FontAwesome className="image-icon" name="expand" title="expand"/>
+      >
+        <div style={{transform: `rotate(${-this.state.rotation }deg)`}}>
+          <FontAwesome className="image-icon" name="sync-alt" title="rotate" onClick={this.rotateImage}/>
+          <FontAwesome className="image-icon" name="trash-alt" title="delete" onClick={this.deleteImage.bind(this)}/>
+          <FontAwesome className="image-icon" name="expand" title="expand" onClick={()=> this.setState({isGalleryMode:true,photoIndex: this.findIndexById()})}/>
+          {isGalleryMode &&(
+            <LightBox
+              mainSrc={`${this.urlFromDto(this.props.images[photoIndex])}`}
+              nextSrc={`${this.urlFromDto(this.props.images[(photoIndex + 1) % this.props.images.length])}`}
+              prevSrc={`${this.urlFromDto(this.props.images[(photoIndex + this.props.images.length - 1) % this.props.images.length])}`}
+              onCloseRequest={() => this.setState({ isGalleryMode: false })}
+              onMovePrevRequest={() =>
+                this.setState({
+                  photoIndex: (photoIndex + this.props.images.length - 1) % this.props.images.length
+                })
+              }
+              onMoveNextRequest={() =>
+                this.setState({
+                  photoIndex: (photoIndex + 1) % this.props.images.length
+                })
+              }
+              clickOutsideToClose={true}
+              imageTitle={this.props.images[photoIndex].title}
+            />
+        )}
         </div>
       </div>
     );
   }
+
+
 }
 
 export default Image;
