2abf7459781cd8c6b42363cfc86f30703684787e
eyalrozenberg <37139238+eyalrozenberg@users.noreply.github.com>
2018-03-12 23:11:00 +0200

    Add files via upload

--- src/components/Image/Image.js
+++ src/components/Image/Image.js
@@ -3,44 +3,49 @@ import PropTypes from 'prop-types';
 import FontAwesome from 'react-fontawesome';
 import './Image.scss';
 
-
-
+ 
 class Image extends React.Component {
   static propTypes = {
     dto: PropTypes.object,
     galleryWidth: PropTypes.number
   };
 
-  
   constructor(props) {
     super(props);
     this.calcImageSize = this.calcImageSize.bind(this);
-	this.rotate = this.rotate.bind(this);
+    this.rotate = this.rotate.bind(this);
     this.state = {
-	  rotation: 0,
+                  rotation: 0,
       size: 200
     }
   }
 
+ 
   calcImageSize() {
-    const {galleryWidth} = this.props;
+    //const {galleryWidth} = this.props;
+const galleryWidth = document.body.clientWidth;
     const targetSize = 200;
-    const imagesPerRow = Math.round(galleryWidth / targetSize);	
+    const imagesPerRow = Math.round(galleryWidth / targetSize);              
     const size = (galleryWidth / imagesPerRow);
     this.setState({
       size
     });
   }
 
+  resize(){
+  this.calcImageSize();
+  }
+
   componentDidMount() {
     this.calcImageSize();
+window.addEventListener("resize", this.resize.bind(this));
   }
 
   urlFromDto(dto) {
     return `https://farm${dto.farm}.staticflickr.com/${dto.server}/${dto.id}_${dto.secret}.jpg`;
   }
-  
-    rotate(){
+
+ rotate(){
     let newRotation = this.state.rotation + 90;
     if(newRotation >= 360){
       newRotation = 0;
@@ -49,19 +54,19 @@ class Image extends React.Component {
       rotation: newRotation
     })
   }
-  
+
   render() {
-	const { showing } = this.state;
-	const { rotation } =  this.state;
+                const { showing } = this.state;
+                const { rotation } =  this.state;
     return (
       <div
         className="image-root"
         style={{
-		  display: (showing ? 'none' : 'inline-block'),
+                                  display: (showing ? 'none' : 'inline-block'),
           backgroundImage: `url(${this.urlFromDto(this.props.dto)})`,
           width: this.state.size,
           height: this.state.size + 'px',
-		  transform: `rotate(${rotation}deg)`
+                                  transform: `rotate(${rotation}deg)`
         }}
         >
         <div style={{transform: `rotate(${rotation*3}deg)`}}>
@@ -74,6 +79,4 @@ class Image extends React.Component {
   }
 }
 
-export default Image;
-
-
+ export default Image;
