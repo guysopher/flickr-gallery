cfed32cc77570227975ba204afa8e5f83234c0a7
galezra <galezra@gmail.com>
2018-03-12 16:26:07 +0200

    Add infinite scroll + loading icon & fix search result while tag changing

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
 import Image from '../Image';
+import FontAwesome from 'react-fontawesome';
 import './Gallery.scss';
 
 class Gallery extends React.Component {
@@ -14,7 +15,8 @@ class Gallery extends React.Component {
     this.state = {
       images: [],
       galleryWidth: this.getGalleryWidth(),
-      loadImages: false
+      loadImages: false,
+      page: 1
     };
   }
 
@@ -27,7 +29,7 @@ class Gallery extends React.Component {
   }
 
   getImages(tag) {
-    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=100&format=json&nojsoncallback=1`;
+    const getImagesUrl = `services/rest/?method=flickr.photos.search&api_key=522c1f9009ca3609bcbaf08545f067ad&tags=${tag}&tag_mode=any&per_page=100&page=${this.state.page}&format=json&nojsoncallback=1`;
     const baseUrl = 'https://api.flickr.com/';
     axios({
       url: getImagesUrl,
@@ -42,7 +44,7 @@ class Gallery extends React.Component {
           res.photos.photo &&
           res.photos.photo.length > 0
         ) {
-          this.setState({images: res.photos.photo});
+          this.setState({images:this.state.images.concat(res.photos.photo),loadImages:false,page:this.state.page+1});
         }
       });
   }
@@ -51,27 +53,58 @@ class Gallery extends React.Component {
     this.getImages(this.props.tag);
     this.setState({galleryWidth: document.body.clientWidth});
     window.addEventListener('resize',  ()=> this.setState({galleryWidth:this.getGalleryWidth()}));
+    window.addEventListener('scroll',this.scrollHandler.bind(this));
   }
 
   componentWillUnmount(){
     window.removeEventListener('resize', ()=> this.setState({galleryWidth:this.getGalleryWidth()}));
+    window.removeEventListener('scroll',this.scrollHandler.bind(this));
   }
 
   componentWillReceiveProps(props) {
+    if(this.props.tag !== props.tag){
+      this.setState({images: []})
+    }
     this.getImages(props.tag);
   }
 
-  updateNewGallery(newImage){
-    this.setState({images: newImage});
+  scrollHandler(){
+    let scrollTop = (document.documentElement && document.documentElement.scrollTop) || document.body.scrollTop;
+    let scrollHeight = (document.documentElement && document.documentElement.scrollHeight) || document.body.scrollHeight;
+    let clientHeight = document.documentElement.clientHeight || window.innerHeight;
+    let scrolledToBottom = Math.ceil(scrollTop + clientHeight) >= scrollHeight;
+
+    if (scrolledToBottom) {
+      if(this.state.loadImages){
+        return;
+      }
+
+      setTimeout(this.getImages(this.props.tag),5000);
+      this.setState({loadImages:true})
+
+    }
+  }
+
+  updateNewGallery(newImagesArray){
+    this.setState({images: newImagesArray});
   }
 
   render() {
     return (
       <div className="gallery-root">
         {this.state.images.map(dto => {
-          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth} images={this.state.images} updateNewGallery={this.updateNewGallery.bind(this)}/>;
+          return <Image key={'image-' + dto.id} dto={dto} galleryWidth={this.state.galleryWidth} images={this.state.images}  updateNewGallery={this.updateNewGallery.bind(this)}/>;
         })}
+        {(() => {
+          if (this.state.loadImages) {
+            return(
+              <div className="data-loading">
+              <FontAwesome className="image-icon" size="4x" name="sync-alt" spin/>
+              </div>
+            );
+          }})()}
       </div>
+
     );
   }
 }
