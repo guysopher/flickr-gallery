a24291d191f77a0f6664cd21fa0ca216c1d3ec9b
itai.meushar <meushar421@gmail.com>
2019-06-28 21:05:00 +0300

    draft - wait for end of user input

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,9 +8,28 @@ class App extends React.Component {
 
   constructor() {
     super();
+    this.timeout = null;
     this.state = {
       tag: 'art'
     };
+  }
+
+  handleInput = (e) => {
+    const input = e.target.value;
+    clearTimeout(this.timeout);
+
+    this.timeout = setTimeout(() => {
+      this.setState({tag: input});
+    }, 500);
+  }
+
+  handleKeyPress = (e) => {
+    const keyCode = e.keyCode || e.which;
+    const enterKeyCode = 13;
+
+    if(keyCode === enterKeyCode) {
+      this.setState({tag: e.target.value});
+    }
   }
 
   render() {
@@ -18,7 +37,10 @@ class App extends React.Component {
       <div className="app-root">
         <div className="app-header">
           <h2>Flickr Gallery</h2>
-          <input className="app-input" onChange={event => this.setState({tag: event.target.value})} value={this.state.tag}/>
+          <input className="app-input"
+            onChange={this.handleInput}
+            onKeyPress={this.handleKeyPress}
+          />
         </div>
         <Gallery tag={this.state.tag}/>
       </div>
